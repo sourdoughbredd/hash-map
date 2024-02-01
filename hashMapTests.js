feeac9da9204588f6a3b5700ce584a266bded6db
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-02-01 11:08:37 -0500

    Add tests for isEmpty()

--- hashMapTests.js
+++ hashMapTests.js
@@ -6,7 +6,8 @@ import { HashMap } from "./hashMap.js";
 console.log("TESTING INITIALIZATION...");
 const hm = new HashMap();
 console.log(hm.toString());
-
+console.log("isEmpty() returns " + hm.isEmpty());
+console.log();
 // Put some stuff in
 console.log("TESTING PUT() WITH A NEW KEY...");
 console.log("Adding some entries...");
@@ -17,12 +18,15 @@ hm.set("four", "cuatro");
 hm.set("five", "cinco");
 hm.set("six", "says");
 console.log(hm.toString());
+console.log("isEmpty() returns " + hm.isEmpty());
+console.log();
 
 // Update a key
 console.log("TESTING PUT() WITH AN EXISTING KEY...");
 console.log("Updating key='six'...");
 hm.set("six", "seis");
 console.log(hm.toString());
+console.log();
 
 // Fill until resize
 console.log("TESTING RESIZING...");
@@ -37,12 +41,15 @@ console.log(hm.toString());
 console.log("AFTER RESIZE:");
 hm.set(i.toString(), i.toString());
 console.log(hm.toString());
+console.log();
 
 // Reset
 console.log("TESTING CLEAR() AND RESETTING MAP...");
 console.log("Clearing hash map using clear()...");
 hm.clear();
 console.log(hm.toString());
+console.log("isEmpty() returns " + hm.isEmpty());
+
 console.log("Adding some entries...");
 hm.set("one", "uno");
 hm.set("two", "dos");
@@ -51,6 +58,7 @@ hm.set("four", "cuatro");
 hm.set("five", "cinco");
 hm.set("six", "seis");
 console.log(hm.toString());
+console.log();
 
 // Get
 console.log("TESTING GET()...");
@@ -59,12 +67,14 @@ console.log("get('one') returns " + hm.get("one"));
 console.log("get('eighty-two') returns " + hm.get("eighty-two"));
 console.log("Make sure map is unchanged...");
 console.log(hm.toString());
+console.log();
 
 // Has
 console.log("TESTING HAS()...");
 console.log("has('six') returns " + hm.has("six"));
 console.log("has('one') returns " + hm.has("one"));
 console.log("has('eighty-two') returns " + hm.has("eighty-two"));
+console.log();
 
 // Remove
 console.log("TESTING REMOVE()...");
@@ -79,3 +89,4 @@ hm.set("three", "tres");
 console.log(hm.toString());
 console.log("remove('one-hundred') returns " + hm.remove("one-hundred"));
 console.log(hm.toString());
+console.log();
