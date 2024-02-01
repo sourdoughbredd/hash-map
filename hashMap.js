c2e3b30c57d84766f74ab9e8e9e614b8cf97f559
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-02-01 09:06:36 -0500

    Implement set() and toString() methods

--- hashMap.js
+++ hashMap.js
@@ -1,22 +1,22 @@
-import { LinkedList } from "./linked-list.js";
+import { BucketList } from "./bucketList.js";
 
 class HashMap {
   // Static properties
-  static #LOAD_FACTOR = 0.75;
-  static #MIN_SIZE = 16;
+  static #MAX_LOAD_FACTOR = 0.75;
+  static #MIN_CAPACITY = 16;
 
   // Instance properties
-  #buckets = this.#getFreshBuckets();
+  #buckets = this.#getFreshBuckets(HashMap.#MIN_CAPACITY);
   #length = 0;
 
   // Generates hash code for the string key
   hash(key) {
-    typeCheck(key);
+    this.#typeCheckKey(key);
     let hashCode = 0;
 
     const primeNumber = 31;
-    for (let i = 0; i < string.length; i++) {
-      hashCode = primeNumber * hashCode + string.charCodeAt(i);
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
     }
 
     return hashCode;
@@ -25,7 +25,22 @@ class HashMap {
   // Places the key-value pair in the HashMap. If the key is already in the map, it is replaced.
   set(key, value) {
     this.#typeCheckKeyValue(key, value);
-    this.#length += 1;
+
+    // Get hash code for key
+    const hashCode = this.hash(key);
+
+    // Convert to bucket number
+    const bucketNum = hashCode % this.#capacity();
+
+    // Look in bucket and insert key value pair
+    this.#boundsCheck(bucketNum);
+    const bucketList = this.#buckets[bucketNum];
+    const newEntry = bucketList.put(key, value);
+
+    if (newEntry) {
+      this.#length += 1;
+      this.#checkForResize();
+    }
   }
 
   // Gets the value stored in the provided key
@@ -64,11 +79,50 @@ class HashMap {
   // Returns an array of entries in the hash map, where an entry is itself an array, consisting of a key-value pair
   entries() {}
 
+  // toString() for printing
+  toString() {
+    let str = `Number of Buckets = ${this.#capacity()}`;
+    for (let bnum = 0; bnum < this.#capacity(); bnum++) {
+      const bl = this.#buckets[bnum];
+      if (bl.size() == 0) continue;
+      str += `\n ${bnum}: ` + bl.toString();
+    }
+    return str;
+  }
+
   // PRIVATE METHODS
 
   // Get new buckets
-  #getFreshBuckets() {
-    return new Array(HashMap.#MIN_SIZE).fill(new LinkedList());
+  #getFreshBuckets(capacity) {
+    const buckets = new Array(capacity);
+    for (let i = 0; i < buckets.length; i++) {
+      buckets[i] = new BucketList();
+    }
+    return buckets;
+  }
+
+  #capacity() {
+    return this.#buckets.length;
+  }
+
+  #loadFactor() {
+    return this.length() / this.#capacity();
+  }
+
+  #checkForResize() {
+    if (this.#loadFactor() <= HashMap.#MAX_LOAD_FACTOR) return;
+    const newCapacity = 2 * this.#capacity();
+    const newBuckets = this.#getFreshBuckets(newCapacity);
+    for (let bl of this.#buckets) {
+      while (bl.size() > 0) {
+        const { key, value } = bl.pop();
+        const hashCode = this.hash(key);
+        const bucketNum = hashCode % newCapacity;
+        const newBucket = newBuckets[bucketNum];
+        newBucket.put(key, value);
+      }
+    }
+    this.#buckets = newBuckets;
   }
 
   // Throws error if key is the wrong type
@@ -91,7 +145,7 @@ class HashMap {
   }
 
   #boundsCheck(index) {
-    if (index < 0 || index >= this.#buckets.length) {
+    if (index < 0 || index >= this.#capacity()) {
       throw new Error("Trying to access index out of bound");
     }
   }
@@ -110,3 +164,32 @@ console.log(hm.buckets.length);
 
 // Initial length of map is 0
 console.log(hm.length());
+
+// Put some stuff in
+hm.set("one", "uno");
+hm.set("two", "dos");
+hm.set("three", "tres");
+hm.set("four", "cuatro");
+hm.set("five", "cinco");
+hm.set("six", "says");
+
+// Print
+console.log(hm.toString());
+
+// Update a key
+console.log("Updating key='six'...");
+hm.set("six", "seis");
+console.log(hm.toString());
+
+// Fill until resize
+console.log("Filling until resize...");
+let i = 7;
+while (hm.length() < 0.75 * 16) {
+  hm.set(i.toString(), i.toString());
+  i++;
+}
+console.log("BEFORE RESIZE:");
+console.log(hm.toString());
+console.log("AFTER RESIZE:");
+hm.set(i.toString(), i.toString());
+console.log(hm.toString());
